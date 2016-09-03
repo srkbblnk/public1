786f600213655454aaaaf05ee241ab11c97dafc1
Roch Jiri <roch@contech.cz>
2016-09-03 13:35:23 +0200

    Prvni lokalni commit

---
+++ WindowsApplication1/Form1.vb
@@ -0,0 +1,12 @@
+﻿Imports System.IO
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MsgBox("ssss" & GetFileSize("c:\test.bgh"))
+    End Sub
+    Public Function GetFileSize(file_path As String) As Double
+        ''
+        Dim gfsfi As New IO.FileInfo(file_path)
+        Dim gfssize As Long = gfsfi.Length
+        GetFileSize = gfssize
+    End Function
+End Class
